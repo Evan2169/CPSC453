8dd1f07de0cabcdd77ad0bb24b8931833683bb52
Evan2169 <evanhc2169@gmail.com>
2019-09-27 11:17:03 -0600

    Adding abstractions for integrating my shape classes with openGl

---
+++ glUtility/DrawableShape.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+#include "Vector.h"
+#include "Vertex.h"
+
+namespace GLUtility
+{
+	template<int dimensions, typename BaseShape, typename CoordinatePrimitive, typename ColourPrimitive>
+	class DrawableShape
+	{
+	public:
+		DrawableShape(const BaseShape& baseShape, const GLUtility::Colour<ColourPrimitive>& colour);
+		~DrawableShape() = default;
+
+		std::vector<Vertex<CoordinatePrimitive, ColourPrimitive>> vertices() const;
+
+	private:
+		BaseShape baseShape_;
+		GLUtility::Colour<ColourPrimitive> colour_;
+	};
+}
+
+template<int dimensions, typename BaseShape, typename CoordinatePrimitive, typename ColourPrimitive>
+GLUtility::DrawableShape<dimensions, BaseShape, CoordinatePrimitive, ColourPrimitive>::DrawableShape(
+	const BaseShape& baseShape, 
+	const GLUtility::Colour<ColourPrimitive>& colour) 
+	: baseShape_(baseShape)
+	, colour_(colour)
+{
+}
+
+template<int dimensions, typename BaseShape, typename CoordinatePrimitive, typename ColourPrimitive>
+std::vector<GLUtility::Vertex<CoordinatePrimitive, ColourPrimitive>> 
+GLUtility::DrawableShape<dimensions, BaseShape, CoordinatePrimitive, ColourPrimitive>::vertices() const
+{
+	using namespace std;
+
+	vector<GLUtility::Vertex<CoordinatePrimitive, ColourPrimitive>> verts;
+	const auto coordinates = baseShape_.vertices();
+	
+	transform(coordinates.begin(), coordinates.end(), back_inserter(verts), 
+		[=](const decltype(coordinates[0])& nakedVertex)
+		{
+			return Vertex<CoordinatePrimitive, ColourPrimitive>(nakedVertex, colour_);
+		});
+
+	return verts;
+}
---
+++ glUtility/Vertex.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace MathTypes
+{
+	template<int dimensions, typename CoordinatePrimitive> class Vector;
+}
+
+namespace GLUtility
+{
+	template<typename ColourPrimitive>
+	struct Colour
+	{
+		Colour() = default;
+		Colour(ColourPrimitive red, ColourPrimitive green, ColourPrimitive blue) : red(red), green(green), blue(blue)
+		{
+		};
+
+		ColourPrimitive red;
+		ColourPrimitive green;
+		ColourPrimitive blue;
+	};
+
+	template<typename CoordinatePrimitive>
+	struct Position
+	{
+		Position() = default;
+		Position(MathTypes::Vector<2, CoordinatePrimitive> vec) : x(vec.xValue()), y(vec.yValue())
+		{
+		};
+		Position(MathTypes::Vector<3, CoordinatePrimitive> vec) : x(vec.xValue()), y(vec.yValue()), z(z.zValue())
+		{
+		};
+
+		CoordinatePrimitive x;
+		CoordinatePrimitive y;
+		CoordinatePrimitive z;
+	};
+
+	template<typename CoordinatePrimitive, typename ColourPrimitive>
+	struct Vertex
+	{
+		Vertex(Position<CoordinatePrimitive> position, Colour<ColourPrimitive> colour) 
+		: position(position), colour(colour)
+		{
+		};
+
+		Position<CoordinatePrimitive> position;
+		Colour<ColourPrimitive> colour;
+	};
+}
