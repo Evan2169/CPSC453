490b8dc120596c2f5a39789d4234fad233d9fba6
Evan2169 <evanhc2169@gmail.com>
2019-09-30 21:43:15 -0600

    Adding a few more useful matrices

--- glUtility/Matrices.h
+++ glUtility/Matrices.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cmath>
+
 #include "Matrix.h"
+#include "Trigonometry.h"
 
 namespace Matrices
 {
@@ -10,18 +13,41 @@ namespace Matrices
 
 	template<typename CoordinatePrimitive>
 	MathTypes::Matrix<4, 4, CoordinatePrimitive> uniformScaleMatrix3D(CoordinatePrimitive scale);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<3, 3, CoordinatePrimitive> translationMatrix2D(
+		CoordinatePrimitive xDelta,
+		CoordinatePrimitive yDelta);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<4, 4, CoordinatePrimitive> translationMatrix3D(
+		CoordinatePrimitive xDelta,
+		CoordinatePrimitive yDelta,
+		CoordinatePrimitive zDelta);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<3, 3, CoordinatePrimitive> rotate2D(CoordinatePrimitive angleInDegrees);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<4, 4, CoordinatePrimitive> rotateAboutX3D(CoordinatePrimitive angleInDegrees);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<4, 4, CoordinatePrimitive> rotateAboutY3D(CoordinatePrimitive angleInDegrees);
+
+	template<typename CoordinatePrimitive>
+	MathTypes::Matrix<4, 4, CoordinatePrimitive> rotateAboutZ3D(CoordinatePrimitive angleInDegrees);
 }
 
-// template<2, typename CoordinatePrimitive>
-// MathTypes::Matrix<3, 3, CoordinatePrimitive> 
-// Matrices::uniformScaleMatrix(CoordinatePrimitive scale)
-// {
-// 	return MathTypes::Matrix<3, 3, CoordinatePrimitive>({
-// 		{scale, 0,     0},
-// 		{0,     scale, 0},
-// 		{0,     0,     1}
-// 	});
-// }
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<3, 3, CoordinatePrimitive> 
+Matrices::uniformScaleMatrix2D(CoordinatePrimitive scale)
+{
+	return MathTypes::Matrix<3, 3, CoordinatePrimitive>({
+		{scale, 0,     0},
+		{0,     scale, 0},
+		{0,     0,     1}
+	});
+}
 
 template<typename CoordinatePrimitive>
 MathTypes::Matrix<4, 4, CoordinatePrimitive> 
@@ -32,5 +58,60 @@ Matrices::uniformScaleMatrix3D(CoordinatePrimitive scale)
 		{0,     scale, 0,     0},
 		{0,     0,     scale, 0},
 		{0,     0,     0,     1}
+	});
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<3, 3, CoordinatePrimitive> Matrices::translationMatrix2D(
+	CoordinatePrimitive xDelta,
+ 	CoordinatePrimitive yDelta)
+{
+	return MathTypes::Matrix<3, 3, CoordinatePrimitive>({
+		{1, 0, xDelta},
+		{0, 1, yDelta},
+		{0, 0, 1}
+	});
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<4, 4, CoordinatePrimitive> Matrices::translationMatrix3D(
+	CoordinatePrimitive xDelta,
+ 	CoordinatePrimitive yDelta,
+	CoordinatePrimitive zDelta)
+{
+	return MathTypes::Matrix<4, 4, CoordinatePrimitive>({
+		{1, 0, 0, xDelta},
+		{0, 1, 0, yDelta},
+		{0, 0, 1, zDelta},
+		{0, 0, 0, 1}
+	});
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<3, 3, CoordinatePrimitive> Matrices::rotate2D(CoordinatePrimitive angleInDegrees)
+{
+	CoordinatePrimitive angleInRadians = Trigonometry::convertAngleFromDegreesToRadians(angleInDegrees);
+	CoordinatePrimitive cosineOfAngle = static_cast<CoordinatePrimitive>(std::cos(angleInRadians));
+	CoordinatePrimitive sineOfAngle = static_cast<CoordinatePrimitive>(std::sin(angleInRadians));
+
+	return MathTypes::Matrix<3, 3, CoordinatePrimitive>({
+		{cosineOfAngle, -1*sineOfAngle, 0},
+		{sineOfAngle, 	 cosineOfAngle,  0},
+		{0, 				 0, 				  1}
+	});
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<4, 4, CoordinatePrimitive> Matrices::rotateAboutX3D(CoordinatePrimitive angleInDegrees)
+{
+	CoordinatePrimitive angleInRadians = Trigonometry::convertAngleFromDegreesToRadians(angleInDegrees);
+	CoordinatePrimitive cosineOfAngle = static_cast<CoordinatePrimitive>(std::cos(angleInRadians));
+	CoordinatePrimitive sineOfAngle = static_cast<CoordinatePrimitive>(std::sin(angleInRadians));
+
+	return MathTypes::Matrix<4, 4, CoordinatePrimitive>({
+		{1, 0, 				 0,              0},
+		{0, cosineOfAngle, -1*sineOfAngle, 0},
+		{0, sineOfAngle, 	 cosineOfAngle,  0},
+		{0, 0, 				 0, 				  1}
 	});
 }
