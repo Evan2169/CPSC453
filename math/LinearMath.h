6921cb7b7c7ae541829a12295bd89ef05aa5d15b
Evan2169 <evanhc2169@gmail.com>
2020-01-14 21:57:55 -0700

    Adding determinant overloads for larger matrices. Adding matrix inverse function.

--- math/LinearMath.h
+++ math/LinearMath.h
@@ -18,6 +18,10 @@ namespace LinearMath
 	CoordinatePrimitive determinant(const MathTypes::Matrix<dimensions, dimensions, CoordinatePrimitive>& matrix);
 
 	template<int dimensions, typename CoordinatePrimitive>
+	MathTypes::Matrix<dimensions, dimensions, CoordinatePrimitive> inverse(
+		const MathTypes::Matrix<dimensions, dimensions, CoordinatePrimitive>& matrix);
+
+	template<int dimensions, typename CoordinatePrimitive>
 	MathTypes::Vector<dimensions, CoordinatePrimitive> linearInterpolation(
 		const MathTypes::Vector<dimensions, CoordinatePrimitive>& lineStart,
 		const MathTypes::Vector<dimensions, CoordinatePrimitive>& lineEnd,		
@@ -81,6 +85,93 @@ CoordinatePrimitive LinearMath::determinant(const MathTypes::Matrix<3, 3, Coordi
 	return m[0][0]*(m[1][1]*m[2][2] - m[1][2]*m[2][1])
 		  - m[0][1]*(m[1][0]*m[2][2] - m[1][2]*m[2][0])
 		  + m[0][2]*(m[1][0]*m[2][1] - m[1][1]*m[2][0]);
+}
+
+template<typename CoordinatePrimitive>
+CoordinatePrimitive LinearMath::determinant(const MathTypes::Matrix<4, 4, CoordinatePrimitive>& matrix)
+{
+	auto det = [](const MathTypes::Matrix<3, 3, CoordinatePrimitive>& matrix)
+	{
+		return LinearMath::determinant(matrix);
+	};
+	using mat = MathTypes::Matrix<3, 3, CoordinatePrimitive>;
+	auto& m = matrix;
+
+	return m[0][0] * det(mat({{m[1][1], m[1][2], m[1][3]}, {m[2][1], m[2][2], m[2][3]}, {m[3][1], m[3][2], m[3][3]}})) 
+		  - m[0][1] * det(mat({{m[1][0], m[1][2], m[1][3]}, {m[2][0], m[2][2], m[2][3]}, {m[3][0], m[3][2], m[3][3]}}))
+		  + m[0][2] * det(mat({{m[1][0], m[1][1], m[1][3]}, {m[2][0], m[2][1], m[2][3]}, {m[3][0], m[3][1], m[3][3]}}))
+		  - m[0][3] * det(mat({{m[1][0], m[1][1], m[1][2]}, {m[2][0], m[2][1], m[2][2]}, {m[3][0], m[3][1], m[3][2]}}));
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<2, 2, CoordinatePrimitive> LinearMath::inverse(
+	const MathTypes::Matrix<2, 2, CoordinatePrimitive>& matrix)
+{
+	auto det = LinearMath::determinant(matrix);
+	auto& m = matrix;
+
+	CoordinatePrimitive data[2][2] = 
+	{
+		{m[1][1], -1*m[0][1]}, 
+		{-1*m[1][0], m[0][0]}
+	};
+
+	return MathTypes::Matrix<2, 2, CoordinatePrimitive>((1/det) * data);
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<3, 3, CoordinatePrimitive> LinearMath::inverse(
+	const MathTypes::Matrix<3, 3, CoordinatePrimitive>& matrix)
+{
+	auto det = [=](const MathTypes::Matrix<2, 2, CoordinatePrimitive>& matrix)
+	{
+		return LinearMath::determinant(matrix);
+	};
+	using mat = MathTypes::Matrix<2, 2, CoordinatePrimitive>;
+	auto& m = matrix;
+
+	CoordinatePrimitive data[3][3] = 
+	{
+		{det(mat({{m[1][1], m[1][2]}, {m[2][1], m[2][2]}})), -1*det(mat({{m[1][0], m[1][2]}, {m[2][0], m[2][2]}})), det(mat({{m[1][0], m[1][1]}, {m[2][0], m[2][1]}}))},
+		{-1*det(mat({{m[0][1], m[0][2]}, {m[2][1], m[2][2]}})), det(mat({{m[0][0], m[0][2]}, {m[2][0], m[2][2]}})), -1*det(mat({{m[0][0], m[0][1]}, {m[2][0], m[2][1]}}))},
+		{det(mat({{m[0][1], m[0][2]}, {m[1][1], m[1][2]}})), -1*det(mat({{m[0][0], m[0][2]}, {m[1][0], m[1][2]}})), det(mat({{m[0][0], m[0][1]}, {m[1][0], m[1][1]}}))}
+	};
+
+	return (1/LinearMath::determinant(matrix)) * MathTypes::Matrix<3, 3, CoordinatePrimitive>(data).transpose();
+}
+
+template<typename CoordinatePrimitive>
+MathTypes::Matrix<4, 4, CoordinatePrimitive> LinearMath::inverse(
+	const MathTypes::Matrix<4, 4, CoordinatePrimitive>& matrix)
+{
+	auto det = [=](const MathTypes::Matrix<3, 3, CoordinatePrimitive>& matrix)
+	{
+		return LinearMath::determinant(matrix);
+	};
+	using mat = MathTypes::Matrix<3, 3, CoordinatePrimitive>;
+	auto& m = matrix;
+
+	CoordinatePrimitive data[4][4] = 
+	{
+		{det(mat({{m[1][1], m[1][2], m[1][3]}, {m[2][1], m[2][2], m[2][3]}, {m[3][1], m[3][2], m[3][3]}})), 
+			-1*det(mat({{m[1][0], m[1][2], m[1][3]}, {m[2][0], m[2][2], m[2][3]}, {m[3][0], m[3][2], m[3][3]}})), 
+					det(mat({{m[1][0], m[1][1], m[1][3]}, {m[2][0], m[2][1], m[2][3]}, {m[3][0], m[3][1], m[3][3]}})),
+						-1*det(mat({{m[1][0], m[1][1], m[1][2]}, {m[2][0], m[2][1], m[2][2]}, {m[3][0], m[3][1], m[3][2]}}))},
+		{-1*det(mat({{m[0][1], m[0][2], m[0][3]}, {m[2][1], m[2][2], m[2][3]}, {m[3][1], m[3][2], m[3][3]}})), 
+				det(mat({{m[0][0], m[0][2], m[0][3]}, {m[2][0], m[2][2], m[2][3]}, {m[3][0], m[3][2], m[3][3]}})), 
+					-1*det(mat({{m[0][0], m[0][1], m[0][3]}, {m[2][0], m[2][1], m[2][3]}, {m[3][0], m[3][1], m[3][3]}})),
+							det(mat({{m[0][0], m[0][1], m[0][2]}, {m[2][0], m[2][1], m[2][2]}, {m[3][0], m[3][1], m[3][2]}}))},
+		{det(mat({{m[0][1], m[0][2], m[0][3]}, {m[1][1], m[1][2], m[1][3]}, {m[3][1], m[3][2], m[3][3]}})), 
+			-1*det(mat({{m[0][0], m[0][2], m[0][3]}, {m[1][0], m[1][2], m[1][3]}, {m[3][0], m[3][2], m[3][3]}})), 
+					det(mat({{m[0][0], m[0][1], m[0][3]}, {m[1][0], m[1][1], m[1][3]}, {m[3][0], m[3][1], m[3][3]}})),
+				  		-1*det(mat({{m[0][0], m[0][1], m[0][2]}, {m[1][0], m[1][1], m[1][2]}, {m[3][0], m[3][1], m[3][2]}}))},
+		{-1*det(mat({{m[0][1], m[0][2], m[0][3]}, {m[1][1], m[1][2], m[1][3]}, {m[2][1], m[2][2], m[2][3]}})), 
+				det(mat({{m[0][0], m[0][2], m[0][3]}, {m[1][0], m[1][2], m[1][3]}, {m[2][0], m[2][2], m[2][3]}})), 
+					-1*det(mat({{m[0][0], m[0][1], m[0][3]}, {m[1][0], m[1][1], m[1][3]}, {m[2][0], m[2][1], m[2][3]}})),
+							det(mat({{m[0][0], m[0][1], m[0][2]}, {m[1][0], m[1][1], m[1][2]}, {m[2][0], m[2][1], m[2][2]}}))}
+	};
+
+	return (1/LinearMath::determinant(matrix)) * MathTypes::Matrix<4, 4, CoordinatePrimitive>(data).transpose();
 }
 
 template<int dimensions, typename CoordinatePrimitive>
